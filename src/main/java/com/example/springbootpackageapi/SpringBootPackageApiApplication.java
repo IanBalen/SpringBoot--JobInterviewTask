d0b34657461ac9191937680230d21979174a21f2
ianba <ian.balen6@gmail.com>
2023-06-03 12:30:14 +0200

    refactor

--- src/main/java/com/example/springbootpackageapi/SpringBootPackageApiApplication.java
+++ src/main/java/com/example/springbootpackageapi/SpringBootPackageApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.springbootposiljkeapi;
+package com.example.springbootpackageapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPosiljkeApiApplication {
+public class SpringBootPackageApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootPosiljkeApiApplication.class, args);
+        SpringApplication.run(SpringBootPackageApiApplication.class, args);
     }
 
 }
--- src/test/java/com/example/springbootpackageapi/SpringBootPosiljkeApiApplicationTests.java
+++ src/test/java/com/example/springbootpackageapi/SpringBootPosiljkeApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springbootposiljkeapi;
+package com.example.springbootpackageapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
