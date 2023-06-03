e8405c874ccd25b9d709d211be68534f92979c8d
ianba <ian.balen6@gmail.com>
2023-06-03 12:09:40 +0200

    init commit

---
+++ src/main/java/com/example/springbootposiljkeapi/SpringBootPosiljkeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootposiljkeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPosiljkeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPosiljkeApiApplication.class, args);
+    }
+
+}
